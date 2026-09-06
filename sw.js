1ec256aafc10b85729cccbebc885339202b11cb1
Haos1980 <kociamberprezesa@gmail.com>
2026-09-06 07:24:07 +0000

    SW v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "most-armii-v7";
+const CACHE = "most-armii-v8";
 const ASSETS = ["./", "./index.html", "./style.css", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {

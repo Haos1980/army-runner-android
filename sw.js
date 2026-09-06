034446a6249d1ddc60e8ec232d14ed718162ebbc
Haos1980 <kociamberprezesa@gmail.com>
2026-09-06 07:29:57 +0000

    Landscape mode (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "most-armii-v9";
+const CACHE = "most-armii-v10";
 const ASSETS = ["./", "./index.html", "./style.css", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {

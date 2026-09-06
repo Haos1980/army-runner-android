40a15062156cffff91780b71b5488a3548f4ca0d
Haos1980 <kociamberprezesa@gmail.com>
2026-09-06 07:54:25 +0000

    Fix blank screen cache/CSS (sw.js)

--- sw.js
+++ sw.js
@@ -1,36 +1,24 @@
-const CACHE = "most-armii-v11";
-const ASSETS = ["./", "./index.html", "./style.css", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
+const CACHE = "most-armii-v12";
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then(() => self.clients.claim())
   );
 });
 
-// Network-first for JS so speed/art updates always land
+// Always prefer network; fall back to cache only offline
 self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  if (url.pathname.endsWith("game.js") || url.searchParams.has("v")) {
-    e.respondWith(
-      fetch(e.request).then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE).then((c) => c.put(e.request, copy));
-        return res;
-      }).catch(() => caches.match(e.request))
-    );
-    return;
-  }
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => {
-      const copy = res.clone();
-      caches.open(CACHE).then((c) => c.put(e.request, copy));
-      return res;
-    }).catch(() => cached))
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });

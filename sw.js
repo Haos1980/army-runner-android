e09d477a5ccd3839ad2ffa1f6cd7f11d79dc46e1
Haos1980 <kociamberprezesa@gmail.com>
2026-09-06 07:17:04 +0000

    SW v5 network-first for game.js

--- sw.js
+++ sw.js
@@ -1,19 +1,8 @@
-const CACHE = "most-armii-v4";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./game.js",
-  "./manifest.json",
-  "./icon.svg",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE = "most-armii-v5";
+const ASSETS = ["./", "./index.html", "./style.css", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener("activate", (e) => {
@@ -24,7 +13,19 @@ self.addEventListener("activate", (e) => {
   );
 });
 
+// Network-first for JS so speed/art updates always land
 self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith("game.js") || url.searchParams.has("v")) {
+    e.respondWith(
+      fetch(e.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => {
       const copy = res.clone();
